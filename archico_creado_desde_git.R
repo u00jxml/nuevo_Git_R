6104fd884059302ea6105fb715dc372a790746a3
u00jxml <jluismartinezram@gmail.com>
2023-03-28 23:03:55 -0600

    continuando ejercitando practica github r

--- archico_creado_desde_git.R
+++ archico_creado_desde_git.R
@@ -4,3 +4,6 @@ b <- 10
 c <- a * b
  
 ##  este archico es un ejercicio de git y R
+### continuamos haciendo pruebas  jose luis 
+## son las 23 hrs de dia 28 marzo 23 continuamos tratando
+## acabar de aprender el tema de git hub con R
