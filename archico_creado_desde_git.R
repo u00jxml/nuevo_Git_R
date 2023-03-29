ecb00121c461f886949a6924645a80ddcf8a0094
u00jxml <jluismartinezram@gmail.com>
2023-03-29 07:45:21 -0600

    joseluis laptop

--- archico_creado_desde_git.R
+++ archico_creado_desde_git.R
@@ -7,3 +7,6 @@ c <- a * b
 ### continuamos haciendo pruebas  jose luis 
 ## son las 23 hrs de dia 28 marzo 23 continuamos tratando
 ## acabar de aprender el tema de git hub con R
+
+#  haciendo cambios con laptop gris 29marzo23  amaneciendo
+d <- 25
